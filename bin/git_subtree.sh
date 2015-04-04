ab8269ea76975ad14a2264908c77e645d69caa95
ksonohara <expertsoftware.i@gmail.com>
2015-04-05 01:33:12 +0900

    new

---
+++ bin/git_subtree.sh
@@ -0,0 +1,11 @@
+# js
+git remote add -f js https://github.com/ksonohara/mikan_js_js.git
+git subtree add --prefix=js/ --squash js master
+
+# css
+git remote add -f css https://github.com/ksonohara/mikan_js_css.git
+git subtree add --prefix=css/ --squash css master
+
+# fonts
+git remote add -f fonts https://github.com/ksonohara/mikan_js_fonts.git
+git subtree add --prefix=fonts/ --squash fonts master
