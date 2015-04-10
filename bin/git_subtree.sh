f4295bd648ce5eae2ab733ffac56db8edffd2754
ksonohara <expertsoftware.i@gmail.com>
2015-04-10 18:14:59 +0900

    git

--- bin/git_subtree.sh
+++ bin/git_subtree.sh
@@ -10,18 +10,41 @@
 #       https://github.com/ksonohara/mikan_js/blob/master/LICENSE
 # 
 #   Module Name:
-#     Boot Console.
+#     Git Command.
 # 
 # ================================================================================
+if [ "_$1" = "_add" ] ; then
+  echo "add"
+  # js
+  git remote add -f js https://github.com/ksonohara/mikan_js_js.git
+  git subtree add --prefix=js/ --squash js master
 
-# js
-git remote add -f js https://github.com/ksonohara/mikan_js_js.git
-git subtree add --prefix=js/ --squash js master
+  # css
+  git remote add -f css https://github.com/ksonohara/mikan_js_css.git
+  git subtree add --prefix=css/ --squash css master
 
-# css
-git remote add -f css https://github.com/ksonohara/mikan_js_css.git
-git subtree add --prefix=css/ --squash css master
+  # fonts
+  git remote add -f fonts https://github.com/ksonohara/mikan_js_fonts.git
+  git subtree add --prefix=fonts/ --squash fonts master
+elif [ "_$1" = "_push" ] ; then
+  echo "push"
+  # js
+  git subtree push --prefix=js/ --squash js master
 
-# fonts
-git remote add -f fonts https://github.com/ksonohara/mikan_js_fonts.git
-git subtree add --prefix=fonts/ --squash fonts master
+  # css
+  git subtree push --prefix=css/ --squash css master
+
+  # fonts
+  git subtree push --prefix=fonts/ --squash fonts master
+else
+  echo "pull"
+  # js
+  git subtree pull --prefix=js --squash js master
+
+  # css
+  git subtree pull --prefix=css --squash css master
+
+  # fonts
+  git subtree pull --prefix=fonts --squash fonts master
+fi
+
